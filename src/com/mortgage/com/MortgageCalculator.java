f3844c7d33b17b991a1d1a1798de5a63db696f05
Kofoworola George-Taylor <43236630+kofogt@users.noreply.github.com>
2022-07-06 16:18:04 +0100

    change file name

--- src/com/mortgage/com/MortgageCalculator.java
+++ src/com/mortgage/com/MortgageCalculator.java
@@ -8,17 +8,37 @@ public class MortgageCalculator {
         mortgage_Calculator();
     }
     public static double mortgage_Calculator(){
+        double principal;
+        double annual_interest_rate;
+        double period_years;
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Principal:  ");
-        double principal = scanner.nextDouble();
-        Scanner scanner1 = new Scanner(System.in);
-        System.out.print("Annual Interest Rate:  ");
-        double annual_interest_rate = scanner.nextDouble();
-        Scanner scanner2 = new Scanner(System.in);
-        System.out.print("Period (Years):  ");
+        while (true){
+            System.out.print("Principal:  ");
+            principal = scanner.nextDouble();
+            if (principal>= 1000 && principal <=2000000){
+                break;
+            }
+            System.out.println("Enter a figure between 1000 and 2000000");
+        }
+        while (true){
+            System.out.print("Annual Interest Rate:  ");
+            annual_interest_rate = scanner.nextDouble();
+            if (annual_interest_rate>= 1 && annual_interest_rate<=100){
+                break;
+            }
+            System.out.println("Enter an interest rate between 1 and 100");
+        }
+        while (true){
+            System.out.print("Period (Years):  ");
+            period_years = scanner.nextDouble();
+            if (period_years>= 1&& period_years <=50){
+                break;
+            }
+            System.out.println("Years should be between 1 and 50.");
+        }
+
         byte months_year = 12;
         byte percentage = 100;
-        double period_years = scanner.nextDouble();
         double number_payments_due = period_years* months_year;
         double monthlyPayment = (annual_interest_rate/percentage)/months_year;
         double formular_top_line = principal * (monthlyPayment )*(Math.pow(1+monthlyPayment, number_payments_due));
