926765158a00b53554ab392cf62b79d3e213111b
Kofoworola George-Taylor <43236630+kofogt@users.noreply.github.com>
2022-07-07 11:52:48 +0100

    updated text

---
+++ src/Tutorial.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Tutorial {
+    public static void main(String[] args) {
+//        whiledope();
+//        calcArea(10,20);
+//        age();
+//        name();
+    }
+//public static void whiledope(){
+//    int[] numbers = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+//    int  loopingThrough = numbers[0];
+//    int loopingLength = numbers.length;
+////    for (int i = 0; i < loopingLength; i++ ){
+////        System.out.println(numbers[i]);
+////    }
+//    int i = 0;
+//    do{
+//        System.out.println(numbers[i]);
+//        i++;
+//    }
+//    while(i<loopingLength);
+//    public static void calcArea(int sidea, int sideb) {
+//    if (sidea >0){
+//        int area = sidea * sideb;
+//        System.out.println( area);
+//public static void age(){
+//        System.out.println("How old are you?");
+//        Scanner scanner  = new Scanner(System.in);
+//        int age = scanner.nextInt();
+//        System.out.println("You are "+ age +" years old.");}
+
+//    public static void name(){
+//        Scanner scanner  = new Scanner(System.in);
+//        System.out.print("What is your name:  ");
+//        String name = scanner.nextLine();
+//        System.out.println("Your name is  "+ name);
+//}
+}
